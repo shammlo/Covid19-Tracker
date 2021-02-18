9a30cc34398a6c0879c9e81dbdc45824b3070e04
sham <shamlo_redzx@yahoo.com>
2021-02-18 22:43:47 +0300

    Development

---
+++ components/Navigation/TopNavigation/TopNavigation.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//******************************** */
+const TopNavigation = () => {
+    return (
+        <nav className="top-navigation">
+            <div>I am a top navigation</div>
+        </nav>
+    );
+};
+
+export default TopNavigation;
