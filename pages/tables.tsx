25b4078977010935642420566bcbc46ffb1e4e1d
sham <shamlo_redzx@yahoo.com>
2021-02-19 22:53:19 +0300

    Development

---
+++ pages/tables.tsx
@@ -0,0 +1,9 @@
+//********** IMPORTS ************* */
+import React from 'react';
+import Tables from '../containers/Tables/Tables';
+//******************************** */
+const tables = () => {
+    return <Tables />;
+};
+
+export default tables;
