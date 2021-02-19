b294e7078afcfc37d73f7f2c8695287e24c71cce
sham <shamlo_redzx@yahoo.com>
2021-02-19 22:52:58 +0300

    Development

--- components/Navigation/SideNavigation/SideNavigation.tsx
+++ components/Navigation/SideNavigation/SideNavigation.tsx
@@ -1,13 +1,14 @@
 //********** IMPORTS ************* */
 import React from 'react';
 import NavigationList from './NavigationList/NavigationList';
+import NavigationLogo from '../NavigationLogo/NavigationLogo';
 //******************************** */
 
 const SideNavigation = () => {
     return (
         <header className="header side__navigation">
-            Logo
-            <nav className="navigation">
+            <NavigationLogo src="/virus-1.svg" title="Covid-19 Tracker" href="/" />
+            <nav className="navigation m-t-s">
                 <NavigationList />
             </nav>
         </header>
