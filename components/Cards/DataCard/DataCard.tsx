051fec355c6992d481b0f64afaef4fcc801ecc2d
sham <shamlo_redzx@yahoo.com>
2021-02-19 22:52:51 +0300

    Development

--- components/Cards/DataCard/DataCard.tsx
+++ components/Cards/DataCard/DataCard.tsx
@@ -1,76 +1,8 @@
 //********** IMPORTS ************* */
-import React, { useEffect, useRef } from 'react';
-import Chart from 'chart.js';
+import React from 'react';
 //******************************** */
 
 const DataCard = (props: any) => {
-    const canvasRef = useRef<any>(null);
-
-    useEffect(() => {
-        const chartOptions = {
-            ...{
-                maintainAspectRatio: true,
-                responsive: true,
-                legend: {
-                    display: false,
-                },
-                tooltips: {
-                    enabled: false,
-                    custom: false,
-                },
-                elements: {
-                    point: {
-                        radius: 0,
-                    },
-                    line: {
-                        tension: 0.33,
-                    },
-                },
-                scales: {
-                    xAxes: [
-                        {
-                            gridLines: false,
-                            ticks: {
-                                display: false,
-                            },
-                        },
-                    ],
-                    yAxes: [
-                        {
-                            gridLines: false,
-                            scaleLabel: false,
-                            ticks: {
-                                display: false,
-                                isplay: false,
-                                // Avoid getting the graph line cut of at the top of the canvas.
-                                // Chart.js bug link: https://github.com/chartjs/Chart.js/issues/4790
-                                // suggestedMax: Math.max(...props.chartData[0].data) + 1,
-                            },
-                        },
-                    ],
-                },
-            },
-            ...props.chartOptions,
-        };
-
-        const chartConfig = {
-            ...{
-                type: 'line',
-                data: {
-                    ...{
-                        labels: props.chartLabels,
-                    },
-                    ...{
-                        datasets: props.chartData,
-                    },
-                },
-                options: chartOptions,
-            },
-            ...props.chartConfig,
-        };
-
-        new Chart(canvasRef.current, chartConfig);
-    });
     return (
         <div className="card">
             <div className="card__box box-one">
@@ -90,7 +22,6 @@ const DataCard = (props: any) => {
                         </span>
                     </span>
                 </div>
-                <canvas height={120} ref={canvasRef} className={`stats-small `} />
             </div>
         </div>
     );
