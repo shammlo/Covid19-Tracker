d07cb280b15b6d5a78938ea283f1ba1d2b06aaa8
sham <shamlo_redzx@yahoo.com>
2021-02-22 22:23:46 +0300

    Development

--- components/Navigation/TopNavigation/TopNavigation.tsx
+++ components/Navigation/TopNavigation/TopNavigation.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import Wrapper from '../../../helpers/Hoc/Wrappers/Wrapper';
+import Button from '../../../helpers/Hoc/Buttons/Button';
 //******************************** */
-const TopNavigation = () => {
+const TopNavigation = (props: any) => {
     return (
-        <nav className="top-navigation">
-            <div>I am a top navigation</div>
+        <nav className="top__navigation">
+            <Wrapper class="top__navigation--content">
+                <Button class="top__navigation--button" clicked={() => props.showSideNavigation()}>
+                    <Wrapper class="top__navigation--toggle">
+                        <Wrapper></Wrapper>
+                    </Wrapper>
+                </Button>
+                I am a top navigation
+            </Wrapper>
         </nav>
     );
 };
