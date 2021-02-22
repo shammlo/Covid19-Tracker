880d9417b22e70c1676329c7d04a53f17cc09868
sham <shamlo_redzx@yahoo.com>
2021-02-22 22:23:33 +0300

    Development

--- components/Navigation/SideNavigation/SideNavigation.tsx
+++ components/Navigation/SideNavigation/SideNavigation.tsx
@@ -4,12 +4,19 @@ import NavigationList from './NavigationList/NavigationList';
 import NavigationLogo from '../NavigationLogo/NavigationLogo';
 //******************************** */
 
-const SideNavigation = () => {
+const SideNavigation = (props: any) => {
     return (
-        <header className="header side__navigation">
-            <NavigationLogo src="/virus-1.svg" title="Covid-19 Tracker" href="/" />
+        <header
+            className={`header side__navigation ${props.show ? 'hidden-sideNav' : 'show-sideNav'}`}
+        >
+            <NavigationLogo
+                src="/virus-1.svg"
+                title="Covid-19 Tracker"
+                href="/"
+                show={props.show}
+            />
             <nav className="navigation m-t-s">
-                <NavigationList />
+                <NavigationList show={props.show} />
             </nav>
         </header>
     );
