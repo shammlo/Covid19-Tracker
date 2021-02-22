3429bc1ae4984076e738532038bd54e1ced296bb
sham <shamlo_redzx@yahoo.com>
2021-02-22 22:23:55 +0300

    Development

--- Layout/Layout.tsx
+++ Layout/Layout.tsx
@@ -1,17 +1,20 @@
 //********** IMPORTS ************* */
-import React from 'react';
+import React, { useState } from 'react';
 import SideNavigation from '../components/Navigation/SideNavigation/SideNavigation';
 import TopNavigation from '../components/Navigation/TopNavigation/TopNavigation';
 //******************************** */
 const Layout = (props: any) => {
+    const [sideNavigation, setSideNavigation] = useState(false);
+
+    const showSideNavigation = () => {
+        setSideNavigation(!sideNavigation);
+    };
     return (
         <div className="app">
             <div className="layout">
-                <SideNavigation />
-                <main className="main__container">
-                    <TopNavigation />
-                    {props.children}
-                </main>
+                <SideNavigation show={sideNavigation} />
+                <TopNavigation showSideNavigation={showSideNavigation} />
+                <main className="main__container">{props.children}</main>
             </div>
         </div>
     );
