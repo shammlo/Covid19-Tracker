a142e6b2366924c2e12793afc6a4d56f69202dcd
sham <shamlo_redzx@yahoo.com>
2021-02-22 22:23:29 +0300

    Development

--- components/Navigation/NavigationLogo/NavigationLogo.tsx
+++ components/Navigation/NavigationLogo/NavigationLogo.tsx
@@ -6,6 +6,7 @@ interface Props {
     src: string;
     title: string;
     href: string;
+    show?: boolean;
 }
 const NavigationLogo = (props: Props) => {
     return (
@@ -13,7 +14,9 @@ const NavigationLogo = (props: Props) => {
             <Link href={props.href}>
                 <a className="logo__link">
                     <img className="logo__link--img" src={props.src} width={20} height={20} />
-                    <h2 className="logo__link--title">{props.title}</h2>
+                    <h2 className={`logo__link--title ${props.show ? 'd-no' : 'd-in'}`}>
+                        {props.title}
+                    </h2>
                 </a>
             </Link>
         </div>
