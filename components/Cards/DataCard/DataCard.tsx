68dae4dddadf34c2dc1309e0cdd604abb49a8683
sham <shamlo_redzx@yahoo.com>
2021-02-23 21:07:14 +0300

    Development

--- components/Cards/DataCard/DataCard.tsx
+++ components/Cards/DataCard/DataCard.tsx
@@ -5,24 +5,22 @@ import Wrapper from '../../../helpers/Hoc/Wrappers/Wrapper';
 
 const DataCard = (props: any) => {
     return (
-        <Wrapper class="card">
-            <Wrapper class="card__box box-one">
-                <Wrapper class="card__box--icon">
-                    {/* <img className="icon" src="img/health.svg" alt="Confirmed Icon" /> */}
-                </Wrapper>
-                <h3 className="heading-3">Total name</h3>
-                <h2 className="heading-2 margin-top-small">amount</h2>
-                <Wrapper class="card__box--info">
-                    <span className="info-increase">
-                        <span id="caseIncrease">00 </span>
+        <Wrapper class="card__box box-one">
+            <Wrapper class="card__box--icon">
+                {/* <img className="icon" src="img/health.svg" alt="Confirmed Icon" /> */}
+            </Wrapper>
+            <h3 className="heading-3">Total name</h3>
+            <h2 className="heading-2 margin-top-small">amount</h2>
+            <Wrapper class="card__box--info">
+                <span className="info-increase">
+                    <span id="caseIncrease">00 </span>
+                </span>
+                <span className="info-text">
+                    from yesterday
+                    <span id="totalYesterday" className="yel">
+                        (00)
                     </span>
-                    <span className="info-text">
-                        from yesterday
-                        <span id="totalYesterday" className="yel">
-                            (00)
-                        </span>
-                    </span>
-                </Wrapper>
+                </span>
             </Wrapper>
         </Wrapper>
     );
