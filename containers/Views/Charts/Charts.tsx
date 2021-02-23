bbe6f028bbc6ca8d5d009807d8a2f3aa491faea1
sham <shamlo_redzx@yahoo.com>
2021-02-23 21:07:45 +0300

    Development

---
+++ containers/Views/Charts/Charts.tsx
@@ -0,0 +1,22 @@
+//********** IMPORTS ************* */
+import React from 'react';
+import Head from 'next/head';
+//******************************** */
+const charts = () => {
+    return (
+        <>
+            <Head>
+                <link rel="shortcut icon" href="/virus-1.svg" />
+                <title>Charts</title>
+            </Head>
+            <div className="home">
+                {' '}
+                <div className="page" style={{ marginBottom: '1rem' }}>
+                    <h1>Hi this is Charts page</h1>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default charts;
