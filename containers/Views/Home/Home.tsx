8b6f87bfd51b3ef0537b02650ad26488543d7d9d
sham <shamlo_redzx@yahoo.com>
2021-02-26 22:11:51 +0300

    Development

--- containers/Views/Home/Home.tsx
+++ containers/Views/Home/Home.tsx
@@ -3,9 +3,18 @@ import React from 'react';
 import Head from 'next/head';
 import CardData from '../../../components/Cards/DataCard/DataCard';
 import Wrapper from '../../../helpers/Hoc/Wrappers/Wrapper';
+import VaccineChart from '../../../components/Charts/VaccineChart/VaccineChart';
 //******************************** */
 
 const Home = () => {
+    // const day = 1000;
+    // // * 60 * 60 * 24;
+    // var indexVariable = 56;
+    // setInterval(function () {
+    //     indexVariable = (indexVariable + 1) % 361;
+    //     console.log(indexVariable);
+    // }, day);
+
     return (
         <>
             <Head>
@@ -27,12 +36,30 @@ const Home = () => {
 
                 <Wrapper class="data data-content">
                     <Wrapper class="data-analytics">
-                        <Wrapper class="data-analytics--title">Data Analytics Title</Wrapper>
-                        <Wrapper class="data-analytics--chart">Data Analytics Chart</Wrapper>
+                        <Wrapper class="data-analytics--title">
+                            Data Analytics Title for vaccine
+                        </Wrapper>
+                        <Wrapper class="data-analytics--chart">
+                            Data Analytics Chart
+                            <Wrapper class="vaccine-chart">
+                                <VaccineChart />
+                            </Wrapper>
+                        </Wrapper>
                     </Wrapper>
                     <Wrapper class="data-analytics">
                         <Wrapper class="data-analytics--title">Data Analytics Title</Wrapper>
-                        <Wrapper class="data-analytics--chart">Data Analytics Chart</Wrapper>
+                        <Wrapper class="data-analytics--chart">
+                            Data Analytics Chart
+                            <div className="progress">
+                                <div
+                                    className="progress-bar bg-secondary"
+                                    role="progressbar"
+                                    aria-valuenow={75}
+                                    aria-valuemin={0}
+                                    aria-valuemax={100}
+                                ></div>
+                            </div>
+                        </Wrapper>
                     </Wrapper>
                 </Wrapper>
             </section>
