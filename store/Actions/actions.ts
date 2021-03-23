37e5957d552d18f485554501476e28735429e413
sham <shamlo_redzx@yahoo.com>
2021-03-23 22:43:47 +0300

    Redux Actions

---
+++ store/Actions/actions.ts
@@ -0,0 +1,7 @@
+import { VaccineData } from './Vaccine/VaccineAction';
+
+//********** EXPORTS **********/
+export { historyData } from '../Actions/HistoryData/HistoryDataAction';
+export { GlobalData } from './GlobalData/GlobalDataAction';
+export { VaccineData } from './Vaccine/VaccineAction';
+//************************************
