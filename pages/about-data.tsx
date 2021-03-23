2a82b4b2320a41774bae8c211d5ab1a885afc313
sham <shamlo_redzx@yahoo.com>
2021-03-23 22:44:36 +0300

    About Data Page

---
+++ pages/about-data.tsx
@@ -0,0 +1,10 @@
+//********** IMPORTS ************* */
+import React from 'react';
+import AboutData from '../containers/Views/AboutData/AboutData';
+//******************************** */
+
+const aboutData = () => {
+    return <AboutData />;
+};
+
+export default aboutData;
