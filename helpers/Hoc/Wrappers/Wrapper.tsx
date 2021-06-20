479eb97e6152139a36c2dbe6aaa55e57f6aec23f
sham <shamlo_redzx@yahoo.com>
2021-06-20 23:16:06 +0300

    Custom div(Wrapper) Component

--- helpers/Hoc/Wrappers/Wrapper.tsx
+++ helpers/Hoc/Wrappers/Wrapper.tsx
@@ -3,14 +3,24 @@ import React from 'react';
 
 //******************************** */
 interface Props {
-    class?: string;
-    onClick?: () => void;
+    class?: string | undefined;
+    clicked?: () => void;
     style?: React.CSSProperties;
     children?: React.ReactNode;
+    dataType?: string;
+    ariaLabel?: string;
+    id?: string;
 }
-const Wrapper = (props: Props) => {
+const Wrapper: React.FC<Props> = (props) => {
     return (
-        <div className={props.class} onClick={props.onClick} style={props.style}>
+        <div
+            className={props.class!}
+            id={props.id!}
+            onClick={props.clicked!}
+            style={props.style!}
+            data-type={props.dataType}
+            aria-label={props.ariaLabel!}
+        >
             {props.children}
         </div>
     );
