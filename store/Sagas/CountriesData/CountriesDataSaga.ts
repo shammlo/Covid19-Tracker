08296e8d94575defa04c99617b44a1490c7c84eb
sham <shamlo_redzx@yahoo.com>
2021-08-30 18:13:39 +0300

    Redux

--- store/Sagas/CountriesData/CountriesDataSaga.ts
+++ store/Sagas/CountriesData/CountriesDataSaga.ts
@@ -9,7 +9,7 @@ export function* fetchingCountriesData(): any {
         const response = yield axios.get(
             'https://disease.sh/v3/covid-19/countries?yesterday=false&twoDaysAgo=false&sort=cases'
         );
-        console.log(response.data);
+        // console.log(response.data);
         yield put(actions.CountriesData.fetching_Countries_Data_Success(response.data));
     } catch (error) {
         console.error(error);
--- store/Sagas/Vaccine/VaccineSaga.ts
+++ store/Sagas/Vaccine/VaccineSaga.ts
@@ -7,7 +7,7 @@ export function* fetchVaccineData(): any {
     try {
         const response = yield axios.get('https://disease.sh/v3/covid-19/vaccine');
 
-        console.log(response.data);
+        // console.log(response.data);
         yield put(actions.VaccineData.fetch_vaccine_data_success(response.data));
     } catch (error) {
         yield put(actions.VaccineData.fetch_vaccine_data_fail(error));
