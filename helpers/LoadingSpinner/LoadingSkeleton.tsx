ef6ea7feb2982e2c319e26c98df24234b3188296
sham <shamlo_redzx@yahoo.com>
2021-08-30 18:06:51 +0300

    Loading Spinners

---
+++ helpers/LoadingSpinner/LoadingSkeleton.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LoadingSkeleton = () => (
+    <div className="card-skeleton">
+        <div className="description">
+            <div className="line line-1"></div>
+            <div className="line line-2"></div>
+            <div className="line line-3"></div>
+        </div>
+    </div>
+);
+
+export default LoadingSkeleton;
---
+++ helpers/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,8 @@
+//********** IMPORTS **********/
+import React from 'react';
+//************************************
+const loadingSpinner = () => {
+    return <div className="loader">Loading...</div>;
+};
+
+export default loadingSpinner;
