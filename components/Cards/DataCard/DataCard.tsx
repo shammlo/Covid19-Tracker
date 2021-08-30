1e71e1d9068e6968b5a82bfbd8777dea6d0ebad3
sham <shamlo_redzx@yahoo.com>
2021-08-30 18:02:52 +0300

    Data card

--- components/Cards/DataCard/DataCard.tsx
+++ components/Cards/DataCard/DataCard.tsx
@@ -2,57 +2,25 @@
 import React from 'react';
 import Wrapper from '../../../helpers/Hoc/Wrappers/Wrapper';
 import LoadingSkeleton from '../../../helpers/LoadingSpinner/LoadingSkeleton';
-// import Image from 'next/image';
+import {
+    formatNumber,
+    percentageCalc,
+    numberWithCommas,
+} from '../../../helpers/Functions/HelperFunctions';
 //******************************** */
 interface Props {
     name: string;
     todayValue: number;
     yesterdayValue: number;
 }
-
-const helperFunctions = {
-    formatNumber: (number: number) => {
-        if (isNaN(number)) return number;
-
-        if (number < 9999) {
-            return (number / 1000).toFixed(2) + 'k';
-        }
-
-        if (number < 1000000) {
-            return (number / 1000).toFixed(2) + 'K';
-        }
-        if (number < 10000000) {
-            return (number / 1000000).toFixed(2) + 'M';
-        }
-
-        if (number < 1000000000) {
-            return (number / 1000000).toFixed(2) + 'M';
-        }
-
-        if (number < 1000000000000) {
-            return (number / 1000000000).toFixed(2) + 'B';
-        }
-
-        return '1T+';
-    },
-
-    percentageCalc: (today: number, yesterday: number) => {
-        const yesterdayCase = today - yesterday;
-        const yesterdayPercent = parseFloat(((yesterdayCase / today) * 100).toFixed(2));
-
-        return yesterdayPercent;
-        // return Math.abs(yesterdayPercent);
-    },
-};
-
 //----------------------------------------------------------------
-const DataCard = (props: Props) => {
+const DataCard: React.FC<Props> = (props) => {
     let yesterdayNumber, percentage;
     const oldName = props.name.split('_');
     const newName = oldName[0] === 'today' ? oldName.join("'s ") : oldName.join(' ');
     // console.log(newName[0]);
-    yesterdayNumber = helperFunctions.formatNumber(props.yesterdayValue);
-    percentage = helperFunctions.percentageCalc(props.todayValue, props.yesterdayValue);
+    yesterdayNumber = formatNumber(props.yesterdayValue);
+    percentage = percentageCalc(props.todayValue, props.yesterdayValue);
     let classes = percentage < 0 ? 'decrease danger' : 'increase success';
 
     classes =
@@ -80,11 +48,17 @@ const DataCard = (props: Props) => {
         card = (
             <Wrapper class="card__box ta-c">
                 <Wrapper class="card__box--icon">
-                    <img src={`images/${newName}.svg`} alt="image" width="250" height="250" />
+                    <img
+                        className="card__box--img"
+                        src={`images/${newName}.svg`}
+                        alt="image"
+                        width="250"
+                        height="250"
+                    />
                 </Wrapper>
                 <h3 className="card__box--name">{newName}</h3>
                 <h2 className={`card__box--value ${todayClasses}`}>
-                    {props.todayValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                    {numberWithCommas(props.todayValue)}
                 </h2>
                 <Wrapper class="card__box--info d-flex flex-c ai-c">
                     <Wrapper class="percentage-info">
